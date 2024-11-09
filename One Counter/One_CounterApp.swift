02155ae6560d09c0f552d5f75c3c2660ca309a8d
Denis Banach <denis.banach@gmail.com>
2024-11-09 08:44:05 +0100

    Initial Commit

---
+++ One Counter/One_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_CounterApp.swift
+//  One Counter
+//
+//  Created by Denis Banach on 09.11.24.
+//
+
+import SwiftUI
+
+@main
+struct One_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
