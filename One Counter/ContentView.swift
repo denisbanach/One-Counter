435d4601082cf41e4ee4a575af590e68c9f957b4
Denis Banach <denis.banach@gmail.com>
2024-11-13 19:50:29 +0100

    Small changes

--- One Counter/ContentView.swift
+++ One Counter/ContentView.swift
@@ -9,41 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var counter: Int = 0
+    
+    let backgroundGradient = LinearGradient(
+        gradient: Gradient(colors: [Color.pink, Color.yellow]),
+        startPoint: .top, endPoint: .bottom)
+    
     var body: some View {
         
-        Spacer()
-        VStack {
-            
-            Text("")
-            
-        }
+       
+        
+        ZStack {
+            backgroundGradient.ignoresSafeArea()
+            //Color.black.ignoresSafeArea()
+            VStack {
+                
+                Text("\(counter)")
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                
+                HStack{
+                    
+                    Button{
+                        counter-=1
+                    }
+                    label: {
+                        Text("-")
+                            .frame(width: 150, height: 150)
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }.buttonStyle(.borderedProminent)
+                
+                    Button{
+                        counter+=1
+                    }
+                    label: {
+                        Text("+")
+                            .frame(width: 150, height: 150)
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }.buttonStyle(.borderedProminent)
+                }
+                
+                
+            }
+            .background(Color(.background))
+            .padding()
+        
+        }.background(Color.green)
         
         
         //Spacer()
-        
-        HStack{
-            
-            Button{
-                
-            }
-            label: {
-                Text("-")
-                    .frame(width: 150, height: 150)
-                    .font(.title)
-                    .fontWeight(.bold)
-            }.buttonStyle(.borderedProminent)
-        
-            Button{
-                
-            }
-            label: {
-                Text("+")
-                    .frame(width: 150, height: 150)
-                    .font(.title)
-                    .fontWeight(.bold)
-            }.buttonStyle(.borderedProminent)
-        }
-        .padding()
+       
     }
 }
 
